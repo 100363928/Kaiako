781ce02e07f1204c837c25473c89904bdde53960
Alicia Contreras <aliciacg@MacBook-Pro-de-Alicia-2.local>
2019-03-29 16:13:09 +0100

    Fotos Coches

--- micropractica02/src/app/app.component.ts
+++ micropractica02/src/app/app.component.ts
@@ -15,11 +15,12 @@ export class AppComponent {
   
   public EstadoCoche:any = EstadoCoche;
   
-  public coches:Array<Coche>=[	new Coche('renault','scenic',new Date(2007,10,1),new Date(2018,3,1),5000,'coche01.jpg',EstadoCoche.BUENO),
-							new Coche('seat','ibiza',new Date(2003,4,12),new Date(2018,2,1),1200,'coche02.jpg',EstadoCoche.BUENO),
-							new Coche('renault','megane',new Date(2006,12,23),new Date(2018,2,1),3500,'coche03.jpg',EstadoCoche.MALO),
-							new Coche('tesla','model 3',new Date(2006,12,23),new Date(2018,2,1),4000,'',EstadoCoche.BUENO)];
-  
+  public coches:Array<Coche>=[	new Coche('Renault','Scenic',new Date(2007,10,1),new Date(2018,3,1),5000,'coche01.jpg',EstadoCoche.BUENO),
+							new Coche('Seat','Ibiza',new Date(2003,4,12),new Date(2018,2,1),1200,'coche02.jpg',EstadoCoche.BUENO),
+							new Coche('Renault','Megane',new Date(2006,12,23),new Date(2018,2,1),3500,'coche03.jpg',EstadoCoche.MALO),
+							new Coche('Tesla','model 3',new Date(2006,12,23),new Date(2018,2,1),4000,'coche04.jpg',EstadoCoche.BUENO),
+              new Coche('Citroen','c3',new Date(2017,2,13),new Date(2018,3,7),3850,'coche05.jpg',EstadoCoche.MALO)];
+
   public filtro:string='';
    
   public getFiltrados():Array<number>{
