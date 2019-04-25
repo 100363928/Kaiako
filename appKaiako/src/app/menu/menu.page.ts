f8e6dccb953090fda55a1cf61e4921e30da31b3b
faridwa96 <100362273@alumnos.uc3m.es>
2019-04-25 19:30:20 +0200

    Config Page

--- appKaiako/src/app/menu/menu.page.ts
+++ appKaiako/src/app/menu/menu.page.ts
@@ -22,6 +22,10 @@ export class MenuPage implements OnInit {
     {
       title: 'Aceptar solicitud',
       url: '/menu/aceptar-solicitud'
+    },
+    {
+      title: 'Ajustes',
+      url: '/menu/config'
     }
   ];
 
